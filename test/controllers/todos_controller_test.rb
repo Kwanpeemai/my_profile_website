9a1b46506b2341b91fcc33ce0511f2577cf1724f
Kwanpeemai <kwanpuengchai@gmail.com>
2025-08-17 20:18:39 +0700

    feat: Enhance Todos feature with new views, flash messages, and controller tests

---
+++ test/controllers/todos_controller_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+
+class TodosControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @todo = Todo.create!(title: "Test task", status: "doing")
+  end
+
+  test "should get index" do
+    get todos_url
+    assert_response :success
+    assert_select "h1", "🚀 My Academy Quest"
+  end
+
+  test "should create todo with flash notice" do
+    assert_difference("Todo.count", 1) do
+      post todos_url, params: { todo: { title: "New Task" } }
+    end
+    assert_redirected_to todos_path
+    follow_redirect!
+    assert_match "Task added!", response.body
+    # ตรวจสอบว่ามี div flash สีเขียว
+    assert_select "div.bg-green-500", text: "Task added!"
+  end
+
+  # test "should not create invalid todo with flash alert" do
+  #   # สมมติ model validate presence: true สำหรับ title
+  #   assert_no_difference("Todo.count") do
+  #     post todos_url, params: { todo: { title: "" } }
+  #   end
+  #   assert_response :success
+  #   # flash alert มักจะไม่มี redirect แต่เราสามารถเช็ค error div
+  #   assert_select "div#erroำไr_explanation"
+  # end
+
+  test "should update todo status" do
+    patch todo_url(@todo)
+    @todo.reload
+    assert_equal "done", @todo.status
+    assert_redirected_to todos_path
+  end
+
+  test "should destroy todo" do
+    assert_difference("Todo.count", -1) do
+      delete todo_url(@todo)
+    end
+    assert_redirected_to todos_path
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,13 @@
+require "simplecov"
+SimpleCov.start "rails" do
+  add_filter "/test/"       # ไม่เอาไฟล์ test
+  add_filter "/config/"     # ไม่เอาไฟล์ config
+  coverage_dir "coverage"   # ตั้งชื่อโฟลเดอร์ coverage
+end
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
-require "simplecov"
 
 module ActiveSupport
   class TestCase
@@ -14,14 +20,3 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
   end
 end
-
-
-SimpleCov.start "rails" do
-  add_filter "/test/"       # ไม่เอาไฟล์ test
-  add_filter "/config/"     # ไม่เอาไฟล์ config
-  coverage_dir "coverage"   # ตั้งชื่อโฟลเดอร์ coverage
-end
-
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
